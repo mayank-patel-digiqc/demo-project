120d4a7df758377dd08f2471747b0fd9c9070145
Mayank5610 <mayank@digiqc.com>
2025-01-21 15:22:14 +0530

    -iframe commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <iframe
+        title="login"
+        src="http://localhost:3000"
+        style={{
+          width: "80vw",
+          height: "80vh",
+        }}
+        height={500}
+      />
+    </div>
+  );
+};
+
+export default App;
