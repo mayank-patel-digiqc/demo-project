83f349d989dc9c15b2a77f8bb66e747655f65d66
Mayank5610 <mayank@digiqc.com>
2025-01-22 14:42:10 +0530

    -Added referrerPolicy in iframe

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
       <iframe
         title="login"
         src="https://web.feature2.digiqc.com/"
+        referrerPolicy="origin"
         width={1500}
         height={800}
       />
