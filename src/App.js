b7c4f00cea0ae4550ef196ca689d66a85e7def99
Mayank5610 <mayank@digiqc.com>
2025-01-23 11:11:36 +0530

    -removed sandbox

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
         id="iframe"
         title="login"
         src="https://web.feature2.digiqc.com"
-        // sandbox="allow-scripts allow-same-origin allow-forms allow-downloads"
         width={1500}
         height={800}
       />
